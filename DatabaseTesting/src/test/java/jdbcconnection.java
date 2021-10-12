9422feb4f04ba19be2928257b6d09dc8949bb560
sethan.g <sethan.g@LP-5CG1225D1D.HCLT.CORP.HCL.IN>
2021-10-12 18:57:38 +0530

    Database Testing

---
+++ DatabaseTesting/src/test/java/jdbcconnection.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class jdbcconnection {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		 
+		String host="localhost";
+		String port ="3306";
+		
+	Connection con=	DriverManager.getConnection("jdbc:mysql://localhost:3306/Qadbt", "root", "root123");
+	Statement stmt=con.createStatement();  
+	ResultSet rs=stmt.executeQuery("select * from credentials where Scenario='Zerobalanced'");  
+	
+	while(rs.next())
+	{
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://login.salesforce.com/?locale=eu");
+		
+		driver.findElement(By.id("username")).sendKeys(rs.getString("username"));
+		driver.findElement(By.id("password")).sendKeys(rs.getString("password"));
+		driver.findElement(By.id("Login")).click();
+		
+
+	
+	}
+
+}}
---
+++ udemy_java/src/Core_Java_Part3_Collection/Example_Of_HashSet.java
@@ -0,0 +1,34 @@
+package Core_Java_Part3_Collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Example_Of_HashSet {
+	
+	
+	public static void main(String[] args) { 
+		
+
+		//HashSet treeset, LinkedHashset implements Set interface 
+		//does not accept duplicate values  
+		// There is no guarantee elements stored in sequential order..Random order 
+		HashSet<String> hs= new HashSet<String>(); 
+		hs.add("USA");
+		hs.add("UK");
+		hs.add("INDIA");
+		hs.add("he"); 
+		hs.add("she");
+		hs.add("America"); 
+		System.out.println(hs);
+		System.out.println(hs.remove("UK")); 
+		System.out.println(hs.isEmpty()); 
+		System.out.println(hs.size()); 
+		Iterator<String> i=hs.iterator(); 
+		while(i.hasNext()) 
+		{ 
+			System.out.println(i.next());
+			} }
+
+		
+
+}
