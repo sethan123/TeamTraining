6c83798083caf6e1969bde72e16517c85e36c512
sethan.g <sethan.g@LP-5CG1225D1D.HCLT.CORP.HCL.IN>
2021-10-30 12:03:19 +0530

    Cloud Testing

---
+++ Udemy_cloud_testing/src/test/java/sauce_lab_Testing.java
@@ -0,0 +1,47 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+
+public class sauce_lab_Testing {
+
+	public static final String USERNAME = "rahul2";
+
+	  public static final String ACCESS_KEY = "xxxxx";
+
+	  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+
+
+
+	public static void main(String[] args) throws MalformedURLException {
+
+	// TODO Auto-generated method stub
+
+
+
+	DesiredCapabilities caps = DesiredCapabilities.chrome();
+
+	caps.setCapability("platform", "Windows 7");
+
+	caps.setCapability("version", "51.0");
+
+	WebDriver driver=new RemoteWebDriver(new URL(URL), caps);
+
+	driver.get("http://google.com");
+
+	System.out.println(driver.getTitle());
+
+	}
+
+
+
+	}
+
