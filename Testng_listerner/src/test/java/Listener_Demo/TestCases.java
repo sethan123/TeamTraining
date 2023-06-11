28e989d916670377137da7e1b5c48fe80a8746af
setha <sethan15@hotmail.com>
2023-06-11 16:56:39 +0530

    ddd

--- Testng_listerner/src/test/java/Listener_Demo/TestCases.java
+++ Testng_listerner/src/test/java/Listener_Demo/TestCases.java
@@ -3,9 +3,10 @@ package Listener_Demo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 @Listeners(Listener_Demo.ListenerTest.class)
 public class TestCases {
 	
@@ -16,6 +17,7 @@ public class TestCases {
 	public void Login() throws InterruptedException
 	
 	{
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://ksrtc.in/oprs-web/user/add.do");
 		//signin
--- cucumberTutorial/src/stepdefin/testRunner.java
+++ cucumberTutorial/src/stepdefin/testRunner.java
@@ -1,14 +1,18 @@
 package stepdefin;
-
 import org.junit.runner.RunWith;
 
-import cucumber.api.java.it.Date;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
 
 // runner class
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format={"html:output"})
+@CucumberOptions(
+		features="src/featurefiles",
+		glue="StepDefinition", 
+		plugin = {"pretty", "html:target/cucumber-reports"}
+		)
 public class testRunner {
 	
 	
